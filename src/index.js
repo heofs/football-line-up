e396a4940765773c39087f51033266bdfe421bc7
Henning Ofstad <ofstad10@gmail.com>
2018-10-01 17:59:39 +0100

    add sass css index file to import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
+import './styles/index.scss';
+// import "./styles/App.css";
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
