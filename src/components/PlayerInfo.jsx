992e40641f75a20e53b3cc7da3b224809412b050
Henning Ofstad <ofstad10@gmail.com>
2018-10-01 17:58:15 +0100

    clean up code and add comments

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -7,15 +7,15 @@ import whiteshirt from "../images/shirt_white.svg";
 import yellowshirt from "../images/shirt_yellow.svg";
 import greenshirt from "../images/shirt_green.svg";
 
+// Render all information about the player.
 class PlayerInfo extends Component {
     constructor(props) {
         super(props);
-
-        // this.toggle = this.toggle.bind(this);
         this.state = {
             popoverOpen: false
         };
     }
+
     shirt_color = color => {
         switch (color) {
             case "blue":
@@ -30,12 +30,14 @@ class PlayerInfo extends Component {
                 break;
         }
     };
+
     togglePopover() {
         this.setState({
             popoverOpen: !this.state.popoverOpen
         });
     }
 
+    // Remove spacing in player name to create a unique id for the popover function
     createPopoverId(name) {
         if (name) {
             let splitname = name.split(" ");
@@ -45,7 +47,6 @@ class PlayerInfo extends Component {
 
     render() {
         if (this.props.playerDetails) {
-            // console.log(this.props.playerDetails)
             return (
                 <div
                     id={this.createPopoverId(this.props.playerDetails.name)}
@@ -53,6 +54,7 @@ class PlayerInfo extends Component {
                     className={`col-3 player-col px-0 ${this.props.location}`}
                     key={this.props.playerDetails.id}
                 >
+                    {/* Popover for player details */}
                     <Popover
                         placement="bottom"
                         isOpen={this.state.popoverOpen}
@@ -61,25 +63,19 @@ class PlayerInfo extends Component {
                     >
                         <PopoverHeader>{this.props.playerDetails.name}</PopoverHeader>
                         <PopoverBody>
-                            <div>
-                                Games Played: {this.props.playerDetails.played}
-                            </div>
-                            <div>
-                                Goals Scored: {this.props.playerDetails.goals}
-                            </div>
-                            <div>
-                                Yellow Cards: {this.props.playerDetails.yellowCards}
-                            </div>
-                            <div>
-                                Red Cards: {this.props.playerDetails.redCards}
-                            </div>
+                            <div>Games Played: {this.props.playerDetails.played}</div>
+                            <div>Goals Scored: {this.props.playerDetails.goals}</div>
+                            <div>Yellow Cards: {this.props.playerDetails.yellowCards}</div>
+                            <div>Red Cards: {this.props.playerDetails.redCards}</div>
                         </PopoverBody>
                     </Popover>
+                    {/* Player Shirt */}
                     <img
                         src={this.shirt_color(this.props.shirtcolor)}
                         alt=""
                         className="player-shirt p-0 mx-auto"
                     />
+                    {/* Player Name */}
                     <div className="player-name">
                         <span className="badge badge-pill badge-secondary">
                             {this.props.playerDetails.name}
